650a224c15202370185da711cd0378d8fea4e2b6
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-10-24 10:15:22 -0500

    applying css tailwindcss and ref, computed vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
-	<div class="home">
-		<img alt="Vue logo" src="../assets/logo.png" />
-		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+	<div class="container mx-auto">
+		<div class="flex justify-center flex-col items-center">
+			<img alt="Vue logo" src="../assets/logo.png" />
+			<div>
+				<p>Spaces Left: {{ spacesLeft }} out of {{ capacity }}</p>
+				<p>Capacity: {{ capacity }}</p>
+				<button
+					class="mt-10 bg-green-700 hover:bg-green-800 px-5 py-2 text-white rounded-md"
+					@click="increaseCapacity()"
+				>
+					Increase Capacity
+				</button>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, ref, computed } from 'vue';
 
 export default defineComponent({
 	name: 'HomeView',
-	components: {
-		HelloWorld,
+	components: {},
+	setup() {
+		const capacity = ref(3);
+		const attending = ref(['Tim', 'Bob', 'Joe']);
+
+		function increaseCapacity() {
+			capacity.value++;
+		}
+
+		const spacesLeft = computed(() => capacity.value - attending.value.length);
+
+		return { capacity, increaseCapacity, attending, spacesLeft };
 	},
 });
 </script>
