8be16ae91d98355395c18197dd9ef513c4ed08ec
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-10-25 21:25:52 -0500

    TextInput component

---
+++ src/components/global/TextInput.vue
@@ -0,0 +1,33 @@
+<template>
+	<label>{{ label }}</label>
+	<input
+		v-bind="attrs"
+		:type="type"
+		:value="modelValue"
+		:placeholder="label"
+		class="field"
+		@input="
+			$emit('update:modelValue', ($event.target as HTMLInputElement).value)
+		"
+	/>
+</template>
+
+<script setup lang="ts">
+import { useAttrs } from 'vue';
+
+interface IProp {
+	label: string;
+	type?: string;
+	modelValue: string;
+	// $attrs: object;
+}
+
+withDefaults(defineProps<IProp>(), {
+	label: '',
+	type: 'text',
+	modelValue: '',
+});
+
+const attrs = useAttrs();
+console.log(attrs);
+</script>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,100 @@
 <template>
-	<div class="about">
-		<h1>This is an about page</h1>
+	<div class="w-3/5">
+		<h1 class="text-3xl font-bold mb-7">Create an event</h1>
+		<form>
+			<label>Select a category</label>
+			<select v-model="event.category" class="field">
+				<option
+					v-for="option in categories"
+					:value="option"
+					:key="option"
+					:selected="option === event.category"
+				>
+					{{ option }}
+				</option>
+			</select>
+
+			<h3 class="font-bold mt-5 mb-1">Name & describe your event</h3>
+			<TextInput v-model="event.title" label="Title" />
+
+			<TextInput v-model="event.description" label="Description" data-des="1" />
+
+			<h3 class="font-bold mt-5 mb-1">Where is your event?</h3>
+
+			<TextInput v-model="event.location" label="Location" data-loc="56" />
+
+			<pre>{{ event }}</pre>
+
+			<h3 class="font-bold mt-5 mb-1">Are pets allowed?</h3>
+			<div>
+				<input
+					type="radio"
+					class="field-radio"
+					v-model="event.pets"
+					:value="1"
+					name="pets"
+				/>
+				<label>Yes</label>
+			</div>
+
+			<div>
+				<input
+					type="radio"
+					class="field-radio"
+					v-model="event.pets"
+					:value="0"
+					name="pets"
+				/>
+				<label>No</label>
+			</div>
+
+			<h3 class="font-bold mt-5 mb-1">Extras</h3>
+			<div>
+				<input
+					type="checkbox"
+					v-model="event.extras.catering"
+					class="field-checkbox"
+				/>
+				<label>Catering</label>
+			</div>
+
+			<div>
+				<input
+					type="checkbox"
+					v-model="event.extras.music"
+					class="field-checkbox"
+				/>
+				<label>Live music</label>
+			</div>
+			<div class="my-5">
+				<button class="btn" type="submit">Submit</button>
+			</div>
+		</form>
 	</div>
 </template>
+<script setup lang="ts">
+import TextInput from '@/components/global/TextInput.vue';
+import { ref } from 'vue';
+
+const categories = [
+	'sustainability',
+	'nature',
+	'animal welfare',
+	'housing',
+	'education',
+	'food',
+	'community',
+];
+
+const event = ref({
+	category: '',
+	title: '',
+	description: '',
+	location: '',
+	pets: 1,
+	extras: {
+		catering: false,
+		music: false,
+	},
+});
+</script>
