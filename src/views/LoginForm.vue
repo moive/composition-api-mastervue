3bb4d7eaa1693cb1a861bad8c2e1c04705f38715
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-10-31 19:07:09 -0500

    login form

---
+++ src/views/LoginForm.vue
@@ -0,0 +1,16 @@
+<template>
+	<form @submit.prevent="onSubmit">
+		<TextInput label="Email" type="email" class="mb-3" />
+		<TextInput label="Password" type="password" />
+		<ButtonForm type="submit" class="btn mt-5">Submit</ButtonForm>
+	</form>
+</template>
+
+<script setup lang="ts">
+import TextInput from '@/components/global/TextInput.vue';
+import ButtonForm from '@/components/global/ButtonForm.vue';
+
+const onSubmit = () => {
+	console.log('testing ...');
+};
+</script>
