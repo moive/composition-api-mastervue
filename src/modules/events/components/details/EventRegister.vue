db4d03ffebb608bb312d0d906e67ae84b02c2739
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-11-08 00:29:58 -0500

    programmatic navigation: push and replace

--- src/modules/events/components/details/EventRegister.vue
+++ src/modules/events/components/details/EventRegister.vue
@@ -1,12 +1,18 @@
 <template>
 	<p class="text-lg">Register for the event here</p>
+	<button class="btn" @click="register">Register Me</button>
 </template>
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
 import { IEvent } from '../../interfaces/event.types';
 
 interface IProps {
 	event: IEvent;
 }
 
-defineProps<IProps>();
+const props = defineProps<IProps>();
+
+const router = useRouter();
+const register = () =>
+	router.replace({ name: 'EventDetails', params: { id: props.event.id } });
 </script>
--- src/modules/events/components/details/LayoutDetail.vue
+++ src/modules/events/components/details/LayoutDetail.vue
@@ -1,11 +1,11 @@
 <template>
 	<div v-if="event" class="text-center mt-10">
 		<h1 class="text-3xl font-bold mb-3">{{ event.title }}</h1>
-		<nav class="navbar-event">
+		<nav class="navbar-event mb-5">
 			<router-link :to="{ name: 'EventDetails' }">Details</router-link>
-			<span>|</span>
+			<span class="mx-2">|</span>
 			<router-link :to="{ name: 'EventRegister' }">Register</router-link>
-			<span>|</span>
+			<span class="mx-2">|</span>
 			<router-link :to="{ name: 'EventEdit' }">Edit</router-link>
 		</nav>
 		<router-view :event="event"></router-view>
