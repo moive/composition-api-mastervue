4b0edaffa06976e3c7d45aad6aa0908c3dbc1899
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-12-01 23:13:48 -0500

    add flowbite tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {
-      extend: {
-        fontFamily: {
-          'sans': ['Kumbh Sans', ...defaultTheme.fontFamily.sans],
-        },
-      }
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
-}
+	content: [
+		'./public/**/*.html',
+		'./src/**/*.{vue,js,ts,jsx,tsx}',
+		'./node_modules/flowbite/**/*.{js,jsx,ts,tsx}',
+		'node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}',
+		// './node_modules/flowbite/**/*.js',
+	],
+	theme: {
+		extend: {
+			extend: {
+				fontFamily: {
+					sans: ['Kumbh Sans', ...defaultTheme.fontFamily.sans],
+				},
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/forms'), require('flowbite/plugin')],
+};
