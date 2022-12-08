45192939cacb9889f5f1db81e1cb7e010edb0cee
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2022-12-08 16:35:38 -0500

    fix css

--- src/modules/global/components/NavBar.vue
+++ src/modules/global/components/NavBar.vue
@@ -90,7 +90,7 @@
 				class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
 				id="mobile-menu-2">
 				<ul
-					class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+					class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 main-navbar">
 					<li>
 						<router-link
 							:to="{ name: 'home' }"
